b307a30a5a58f6fe09a32a9161a12d1c877b16b6
Alexey Khokhlov <alexeyxo@rocketguys.com>
2017-11-08 01:16:02 +0300

    Swift 4

--- Package.swift
+++ Package.swift
@@ -1,12 +1,29 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "ProtobufGeneratorUtils",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "ProtobufGeneratorUtils",
+            targets: ["ProtobufGeneratorUtils"]),
+        ],
     dependencies: [
-        .Package(url: "https://github.com/alexeyxo/protobuf-swift.git", majorVersion: 3),
-    ]
+        .package(url: "https://github.com/alexeyxo/protobuf-swift.git", from: "4.0.0")
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "ProtobufGeneratorUtils",
+            dependencies: ["ProtocolBuffers"],
+            path: ".",
+            sources: ["Sources"])
+        ]
 )
 
